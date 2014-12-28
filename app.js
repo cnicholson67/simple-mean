5474fb60897829d80e027d1799e66b3cedd0d7f0
Christopher Nicholson <mail@christophernicholson.net>
2014-12-28 08:19:10 -0600

    added simple authentication

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var http = require("http"),
     mime = require('mime'),
     url = require("url"),
     path = require("path"),
-    fs = require("fs");
-
-
+    fs = require("fs"),
+    Users = require('./UserData.js');
  var port = process.argv[2] || 8888;
  var server =  http.createServer(function(request, response) {
   var uri = url.parse(request.url).pathname
@@ -30,6 +29,30 @@ var http = require("http"),
     });
   });
 }).listen(parseInt(port, 10));
-
+ var users = [];
  var io = require('socket.io')(server);
+ io.on('connection', function (socket) {
+     console.log(socket.id);
+     socket.on('register', function (userdata) {
+         Users.addNewAccount(userdata, function (err) {
+             if (err) {
+                 socket.emit('Regfail', { "message": err });
+             } else {
+                 socket.emit('RegistrationResult', userdata);
+             }
+         });
+     });
+     socket.on('authenticate', function (userdata) {
+         Users.manualLogin(userdata.UserName, userdata.UserPass, function (err,user) {
+             if (err) {
+                 socket.emit('Authfail', { "message": err });
+             } else {
+                 socket.emit('Autheticated', user);
+             }
+         });
+     });
+     socket.on('disconnect', function () {
+
+     });
+ });
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
--- js/app.js
+++ js/app.js
@@ -1,8 +1,90 @@
 var socket = io();
-var app = angular.module('App', ['ui.bootstrap']);
-app.controller("Main",function($scope){
-    $scope.Placeholder = "Place Holder Text";
+var app = angular.module('App', ['ui.bootstrap','ngCookies']);
+app.factory("Application",function(){
+	var AppData = {};
+	return AppData;
+});
+app.controller("Main",function($scope, $modal, $cookies, Application){
+    if ($cookies.AppData) {
+        try{
+            $scope.Application = JSON.parse($cookies.AppData);}
+        catch (e) { }
+	}
+	$scope.$watch("Application",function(val){
+		Application = val;
+	});
+	$scope.Placeholder = "Place Holder Text";
+	$scope.register = function () {
+	    var modalInstance = $modal.open({
+	        templateUrl: '/tmpls/register.html',
+	        controller: function ($scope, $modalInstance, items) {
+	            $scope.UserName = "";
+	            $scope.UserPass = "";
+	            socket.on('Regfail', function (data) {
+	                $scope.Message = data.message;
+	                $scope.$apply();
+	            });
+	            socket.on('RegistrationResult', function (data) {
+	                var res = { "IsAuthenticated": true, "User": data };
+	                $modalInstance.close(res);
+	            });
+	            $scope.Register = function () {
+	                socket.emit('register', { "UserName": $scope.UserName, "FirstName": $scope.FirstName, "LastName": $scope.LastName, "Email": $scope.Email, "UserPass": $scope.UserPass });
+	            }
+	            $scope.cancel = function () {
+	                $modalInstance.close();
+	            }
+	        },
+	        resolve: {
+	            items: function () {
+	                return $scope.items;
+	            }
+	        }
+	    });
+	    modalInstance.result.then(function (result) {
+	        $scope.Application = result;
+	        $cookies.AppData = JSON.stringify($scope.Application);
+	    });
+	}
+    $scope.logout = function () {
+    	$scope.Application = null;
+    	$cookies.AppData = null;
+    }
+    $scope.login = function () {
+        var modalInstance = $modal.open({
+            templateUrl: '/tmpls/login.html',
+            controller: function ($scope, $modalInstance, items) {
+                $scope.UserName = "";
+                $scope.UserPass = "";
+                socket.on('Authfail', function (data) {
+                    $scope.Message = data.message;
+                    $scope.$apply();
+                });
+                socket.on('Autheticated', function (data) {
+                    var res = {"IsAuthenticated":true,"Token":data.Token};
+                    $modalInstance.close(res);
+                });
+                $scope.Authenticate = function () {
+                    socket.emit('authenticate', { "UserName": $scope.UserName, "UserPass": $scope.UserPass });
+                }
+                $scope.cancel = function () {
+                    $modalInstance.close();
+                }
+            },
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+        modalInstance.result.then(function (result) {
+        	$scope.Application = result;
+        	$cookies.AppData = JSON.stringify($scope.Application);
+          });
+        
+    }
 });
 app.controller("Menu",function($scope){
     $scope.MenuText = "Here is Menu Text";
+    
 });
